a34544ae09678759d444b5a70e96ac6262370426
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-05-06 22:49:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,100 @@
 window.onload = () => {
 
+    var body = document.getElementsByTagName("body")[0];
+
+
     var play = document.getElementById("play");
     var pause = document.getElementById("pause");
     var lockin = document.getElementById("lockin");
     var player = document.getElementById("player");
 
-    play.onclick = () =>{
+    /* TOGGLE BETWEEN PLAY & PAUSE*/
+
+    play.onclick = () => {
         play.style.display = "none";
         pause.style.display = "block";
 
-        pause.onclick = () =>{
+        pause.onclick = () => {
             pause.style.display = "none";
             play.style.display = "block";
         }
     }
 
+    /* END OF TOGGLE BETWEEN PAUSE & PLAY* /
+
+    
+
+    /* SHOW PLAYER */
 
     lockin.onclick = () => {
 
-        setTimeout(function(){
-        player.style.opacity = "1";
-        },300);
+        setTimeout(function () {
+            player.style.opacity = "1";
+        }, 300);
 
         player.style.display = "flex";
 
     }
 
+    /* END OF SHOW PLAYER*/
 
-    var menu = document.getElementById("menu");
-    var nav = document.getElementById("mobilenav");
 
-    menu.onclick = () =>{
+
+    /* SHOW & HIDE MOBILE MENU*/
+
+    menu.onclick = () => {
+
+        var menu = document.getElementById("menu");
+        var nav = document.getElementById("mobilenav");
 
         menu.classList.toggle("toggle");
 
-        nav.classList.toggle("showmenu");
-        
-        setTimeout(function(){
-            nav.style.height = "100vh";
-        }, 50);
 
-        document.getElementsByTagName("html").classList.toggle("hidescrollbar");
+        if (nav.style.display === "block") {
 
-        
+            nav.style.height = "0vh";
+
+            setTimeout(function () {
+                nav.style.display = "none";
+            }, 500);
+
+
+
+        } else {
+
+            nav.style.display = "block";
+
+            setTimeout(function () {
+                nav.style.height = "100vh";
+            }, 50);
+
+
+        }
 
 
     }
+
+    /* END OF SHOW & HIDE MOBILE MENU*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
